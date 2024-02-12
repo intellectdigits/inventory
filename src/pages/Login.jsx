800ee0cec43c5e0e7fb336c37e870a98bf0fbf90
intellectdigits <115778808+intellectdigits@users.noreply.github.com>
2024-02-12 13:43:40 -0800

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -98,7 +98,7 @@ const handlesubmit = (e) => {
       <div>
         <label for="email" class="block text-sm font-medium leading-6 text-gray-900">Email address</label>
         <div class="mt-2">
-          <input id="text" name="user" onChange={(e)=>setUser(e.target.value)} type="text" value="victor"  required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          <input id="text" name="user" onChange={(e)=>setUser(e.target.value)} type="text"   required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
@@ -110,7 +110,7 @@ const handlesubmit = (e) => {
           </div>
         </div>
         <div class="mt-2">
-          <input id="password" name="password" value="nnamani131" type="password" onChange={(e)=>setPassword(e.target.value)} autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
+          <input id="password" name="password"  type="password" onChange={(e)=>setPassword(e.target.value)} autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"/>
         </div>
       </div>
 
